c080fb3298b8297db7e059fa87de5d1c29d94725
emnKABORE <melchiekab@gmail.com>
2026-03-13 15:27:56 +0100

    Fix platform detection + screenshot protection + chat security update

--- lib/match/chat_screen.dart
+++ lib/match/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter/foundation.dart';
 
 import '../services/security_service.dart';
 import 'matches_screen.dart';
